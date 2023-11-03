fe7afc83ba5a211bcdb004b9f43e717e252fe4da
Snehalscode <146814976+Snehalscode@users.noreply.github.com>
2023-11-03 12:55:48 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+import NLP
+import yolo
+import face_det
+from audio import listen
+import text
+import label_reading
+import call
+
+command = listen()
+
+label = NLP.classify_input(command)
+
+print(label)
+
+if label == 0:
+    text1 = "Hello Neel this is a test"
+    text.text(text1)
+
+elif label == 1:
+    yolo.yolo()
+
+elif label == 2:
+    face_det.get_person()
+
+elif label == 3:
+    call.call()
+
+elif label == 4:
+    label_reading.read_label()
